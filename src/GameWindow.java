6270d95e928bb22888c97713b3455cd06fa2b071
Vitek-1 <vitfrolik@gmail.com>
2026-04-19 13:48:59 +0200

    Created Method StartTimer, changed SetPicture and added method ShowAllMines

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Random;
@@ -7,24 +9,39 @@ import java.util.Random;
 public class GameWindow {
     JFrame frame;
     private int[][] Miny = new int[9][9];
+    private JButton[][] buttons;
     private int numberOfMins = 10;
-
-    public GameWindow() {
-        MineGenerator();
-        CalculateMines();
-    }
+    private boolean Lost = false;
+    private int seconsPassed = 0;
+    private int minutesPassed = 0;
+    private Timer timer;
+    private JLabel timeLabel;
 
     public void StartGame() {
         frame = new JFrame("Minesweeper");
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new GridLayout());
+        frame.setLayout(new BorderLayout());
 
-        JPanel panel2 = new JPanel();
+        JPanel panel = new JPanel();
+        panel.setPreferredSize(new Dimension(frame.getWidth(), 100));
+
+        timeLabel = new JLabel();
+        StartTimer();
+
+        timeLabel.setBackground(Color.white);
+        timeLabel.setFont(new Font("Monospaced", Font.BOLD, 25));
+
+        panel.add(timeLabel);
+        panel.setBorder(BorderFactory.createMatteBorder(0, 0, 5, 0, Color.BLACK));
+        frame.add(panel, BorderLayout.NORTH);
+
+        JPanel panel2 = new JPanel(new GridBagLayout());
         panel2.setLayout(new GridLayout(9, 9, 2, 2));
-        panel2.setOpaque(false);
+        panel2.setBackground(Color.BLACK);
+        panel2.setOpaque(true);
 
-        JButton[][] buttons = new JButton[9][9];
+        buttons = new JButton[9][9];
 
         for (int y = 0; y < 9; y++) {
             for (int x = 0; x < 9; x++) {
@@ -33,23 +50,55 @@ public class GameWindow {
                 final int finalX = x;
                 final int finalY = y;
 
-                buttons[x][y].addMouseListener(new MouseAdapter() {
-                    @Override
-                    public void mouseClicked(MouseEvent e) {
-                        if (SwingUtilities.isLeftMouseButton(e)) {
-                            SetPicture(buttons[finalX][finalY], finalX, finalY);
-                        } else if (SwingUtilities.isRightMouseButton(e)){
-                            ChangeLook.setFlagIcon(buttons[finalX][finalY], 100,100);
-                        }
-                    }
-                });
+                    buttons[x][y].addMouseListener(new MouseAdapter() {
+                        @Override
+                        public void mouseClicked(MouseEvent e) {
 
+                            if (SwingUtilities.isLeftMouseButton(e)) {
+                                SetPicture(buttons[finalX][finalY], finalX, finalY);
+
+                                if (Lost){
+                                    ShowAllMines(buttons[finalX][finalY]);
+                                    timer.stop();
+                                }
+
+                            } else if (SwingUtilities.isRightMouseButton(e)) {
+                                if (Miny[finalX][finalY] != 10) {
+                                    buttons[finalX][finalY].setText("");
+                                    ChangeLook.setFlagIcon(buttons[finalX][finalY], 100, 100);
+                                }
+                            }
+                        }
+                    });
+
+                buttons[x][y].setBorderPainted(false);
+                buttons[x][y].setFocusPainted(false);
                 panel2.add(buttons[x][y]);
             }
         }
-        frame.add(panel2, BorderLayout.SOUTH);
+        frame.add(panel2, BorderLayout.CENTER);
+
+        MineGenerator();
+        CalculateMines();
 
         frame.setVisible(true);
+    }
+
+    public void StartTimer() {
+        timer = new Timer(1000, new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                seconsPassed++;
+                if (seconsPassed == 60) {
+                    seconsPassed = 0;
+                    minutesPassed++;
+                }
+                timeLabel.setText(String.format("%02d:%02d", minutesPassed, seconsPassed));
+                timeLabel.setFont(new Font("Monospaced", Font.BOLD, 50));
+            }
+        });
+        timer.start();
     }
 
     public void MineGenerator() {
@@ -57,7 +106,7 @@ public class GameWindow {
         int laidMines = 0;
 
         while (laidMines < numberOfMins) {
-            int x  = rnd.nextInt(9);
+            int x = rnd.nextInt(9);
             int y = rnd.nextInt(9);
 
             if (Miny[x][y] != 9) {
@@ -79,7 +128,7 @@ public class GameWindow {
                             int neighbourX = x + i;
                             int neighbourY = y + j;
 
-                            if (neighbourX >= 0 && neighbourY >= 0 &&  neighbourX < 9 && neighbourY < 9) {
+                            if (neighbourX >= 0 && neighbourY >= 0 && neighbourX < 9 && neighbourY < 9) {
                                 if (Miny[neighbourX][neighbourY] == 9) {
                                     neighbour++;
                                 }
@@ -92,12 +141,38 @@ public class GameWindow {
         }
     }
 
-    public void SetPicture(JButton button, int x, int y){
-        if (Miny[x][y] != 9) {
+    public void SetPicture(JButton button, int x, int y) {
+        if (Miny[x][y] != 9 && Miny[x][y] != 10) {
+            button.setIcon(null);
             button.setText(String.valueOf(Miny[x][y]));
-            button.setFont(new Font("Times New Roman", Font.BOLD, 50));
-        } else {
-            ChangeLook.setMinaIcon(button, 100,100);
+            button.setFont(new Font("Times New Roman", Font.BOLD, 45));
+            Miny[x][y] = 10;
+
+            button.setBackground(new Color(194, 194, 194));
+            button.setContentAreaFilled(false);
+            button.setOpaque(true);
+
+            button.setBorderPainted(false);
+            button.setFocusPainted(false);
+        } else if (Miny[x][y] == 9) {
+            ChangeLook.setMinaIcon(button, 100, 100);
+            Lost = true;
+        }
+    }
+
+    public void ShowAllMines(JButton button){
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                if (Miny[x][y] == 9) {
+                    ChangeLook.setMinaIcon(buttons[x][y], 100, 100);
+                }
+                buttons[x][y].setBackground(new Color(194, 194, 194));
+                buttons[x][y].setContentAreaFilled(false);
+                buttons[x][y].setOpaque(true);
+
+                buttons[x][y].setBorderPainted(false);
+                buttons[x][y].setFocusPainted(false);
+            }
         }
     }
 
