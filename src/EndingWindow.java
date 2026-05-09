6459ec48af02ff07c9f72cd8feeafbae01248153
Vitek-1 <vitfrolik@gmail.com>
2026-05-09 09:06:53 +0200

    new window at the end

---
+++ src/EndingWindow.java
@@ -0,0 +1,78 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class EndingWindow extends JDialog {
+
+    private boolean finished = false;
+    private String playersName = "";
+
+    public void TheEnd() {
+        setTitle("You won the game!");
+        setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+        setLayout(new BorderLayout(5, 5));
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setBackground(new Color(192, 192, 192));
+        setModal(true);
+
+        JLabel message = new JLabel("    Please type your name: ");
+        message.setFont(new Font("Times New Roman", Font.BOLD, 25));
+        message.setPreferredSize(new Dimension(300, 50));
+        add(message, BorderLayout.NORTH);
+
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.setPreferredSize(new Dimension(getWidth(), getHeight()/2));
+        Border emptySpace = BorderFactory.createEmptyBorder(10, 10, 10, 10);
+        bottomPanel.setBorder(emptySpace);
+
+        JTextField messageText = new JTextField();
+        messageText.setFont(new Font("Times New Roman", Font.BOLD, 30));
+        messageText.setPreferredSize(new Dimension(300, 150));
+        bottomPanel.add(messageText,  BorderLayout.NORTH);
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        submitButton.setFocusPainted(false);
+        bottomPanel.add(submitButton, BorderLayout.CENTER);
+
+        add(bottomPanel, BorderLayout.CENTER);
+
+        submitButton.addActionListener(e ->{
+            Clicked(messageText);
+        });
+
+        messageText.addKeyListener(new  KeyAdapter() {
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+                    Clicked(messageText);
+                }
+            }
+        });
+
+        setVisible(true);
+    }
+
+    public void Clicked(JTextField messageText) {
+        String name = messageText.getText();
+        if (name.isBlank()) {
+            JOptionPane.showMessageDialog(this, "Please enter your name!");
+            messageText.setText("");
+        } else {
+            finished = true;
+            playersName = name;
+            dispose();
+        }
+    }
+
+    public boolean isFinished() {
+        return finished;
+    }
+
+    public String getPlayersName() {
+        return playersName;
+    }
+}
